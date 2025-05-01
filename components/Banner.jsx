7e07286b0eebe1b63b31370ef70f2fc8a2678376
haseebmir07 <haseebrecovery393@gmail.com>
2025-05-02 02:30:23 +0530

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -32,7 +32,7 @@ const Banner = () => {
       />
       <Image
         className="md:hidden"
-        src={assets.sm_controller_image}
+        src={assets.stuff}
         alt="sm_controller_image"
       />
     </div>
